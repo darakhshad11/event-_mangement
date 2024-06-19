f7718c904680c7f3531ab4da9a8beb6addd2f572
darakhsha rayen <darakhsharayen9@gmail.com>
2024-06-19 19:26:03 +0530

    done

--- src/data.js
+++ src/data.js
@@ -1,5 +1,5 @@
 export const users = [
-    { username: 'vinay', pass: '12345678', role: 'user' },
+    { username: 'darakhsha', pass: '12345678', role: 'user' },
     { username: 'Text', pass: 'abcd', role: 'vendor' },
   ];
   
